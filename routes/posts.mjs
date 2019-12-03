36abaf640f29bb361409d92be3b6482e54be8bfa
DeAndre Johnson <dre@phreshr.com>
2019-12-04 04:34:04 +0800

    feature: GET posts by id

--- routes/posts.mjs
+++ routes/posts.mjs
@@ -16,4 +16,25 @@ router.get("/", async (req, res) => {
   }
 });
 
+// Get post by ID
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const post = await db.findById(id);
+
+    if (!post)
+      return res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+
+    res.status(200).json(post);
+  } catch (error) {
+    console.log("The posts information could not be retrieved.", error);
+    res
+      .status(500)
+      .json({ error: "The post information could not be retrieved." });
+  }
+});
+
 export default router;
